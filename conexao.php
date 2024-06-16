5f57372823610f961cd44fdf671d208e1e510542
SamuelRamos12 <samueljorge799@gmail.com>
2024-06-16 11:52:55 -0300

    Atualizações e realizado lista suspensa

--- conexao.php
+++ conexao.php
@@ -10,4 +10,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
+$conn->set_charset("utf8");
 ?>
---
+++ lista.php
@@ -0,0 +1,22 @@
+<?php
+// Incluir o arquivo de conexão
+require_once 'conexao.php';
+
+// Consulta SQL para obter as categorias
+$sql = "SELECT IP, Tipo FROM equipamentos";
+$resultado = $conn->query($sql);
+
+$categorias = array();
+
+if ($resultado->num_rows > 0) {
+    while ($row = $resultado->fetch_assoc()) {
+        $categorias[] = $row;
+    }
+}
+
+// Fechar conexão
+$conn->close();
+
+// Retornar os resultados como JSON
+echo json_encode($categorias);
+?>
